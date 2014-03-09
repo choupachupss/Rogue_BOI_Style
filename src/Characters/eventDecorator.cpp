324166619e2bcbbaa949c89bcc443596951238b0
Colombiman <colombiman@hotmail.com>
2014-03-09 07:57:00 +0100

    miaou

--- src/Characters/eventDecorator.cpp
+++
@@ -1,7 +0,0 @@
-#include "eventDecorator.h"
-
-void eventDecorator::collide(const Hittable* e)
-{
-	Notify(Events::LeaveRoom(), NORTH);
-	component->collide(e); //Chain for other decorators
-}
--- src/Views/Rebind_inputs_menu.cpp
+++
@@ -1,6 +0,0 @@
-#include "Rebind_inputs_menu.h"
-
-Rebind_Inputs_Menu::Rebind_Inputs_Menu(sf::RenderTarget& target, Input::GameInput& g) :
-g_i(g),
-windows(target)
-{}
